4617b0708ce7c4a6f5c3d4bafde52048a0d0eca0
hindenbug <manoj.mk27@gmail.com>
2015-01-16 23:04:18 +0530

    save progress

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,36 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12,
+                   \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[f _] card]
+    (if (Character/isDigit f)
+      (Integer/valueOf (str f))
+      (replacements f))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (<= 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (<= 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (<= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= [1 2 3] (sort (vals (frequencies (map rank hand)))))
+      (= [1 4] (sort (vals (frequencies (map rank hand)))))))
 
 (defn straight? [hand]
   nil)
